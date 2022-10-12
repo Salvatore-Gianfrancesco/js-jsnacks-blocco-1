97340ffb2015dac869828e6de1946a07589decc4
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-10-12 11:42:40 +0200

    added isNaN control on input

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -6,13 +6,19 @@ const num2 = prompt("Inserisci il secondo numero");
 
 const h1El = document.querySelector("h1");
 
-if (num1 > num2) {
-    console.log(`Il maggiore tra i due numeri inseriti è il primo: ${num1}`);
-    h1El.innerHTML = `Il maggiore tra i due numeri inseriti è il primo: ${num1}`;
-} else if (num1 < num2) {
-    console.log(`Il maggiore tra i due numeri inseriti è il secondo: ${num2}`);
-    h1El.innerHTML = `Il maggiore tra i due numeri inseriti è il secondo: ${num2}`;
+if (isNaN(num1) || isNaN(num2)) {
+    console.log(`Almeno uno dei due valori inseriti non è un numero`);
+    h1El.innerHTML = `Almeno uno dei due valori inseriti non è un numero`;
 } else {
-    console.log(`I numeri sono uguali`);
-    h1El.innerHTML = `I numeri sono uguali`;
+    if (num1 > num2) {
+        console.log(`Il maggiore tra i due numeri inseriti è il primo: ${num1}`);
+        h1El.innerHTML = `Il maggiore tra i due numeri inseriti è il primo: ${num1}`;
+    } else if (num1 < num2) {
+        console.log(`Il maggiore tra i due numeri inseriti è il secondo: ${num2}`);
+        h1El.innerHTML = `Il maggiore tra i due numeri inseriti è il secondo: ${num2}`;
+    } else {
+        console.log(`I numeri sono uguali`);
+        h1El.innerHTML = `I numeri sono uguali`;
+    }
 }
+
