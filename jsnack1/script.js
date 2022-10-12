d86fd6f29599cdae0acb03e6981ea512eca880fb
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-10-12 11:39:39 +0200

    added DOM

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -4,10 +4,15 @@
 const num1 = prompt("Inserisci il primo numero")
 const num2 = prompt("Inserisci il secondo numero");
 
+const h1El = document.querySelector("h1");
+
 if (num1 > num2) {
     console.log(`Il maggiore tra i due numeri inseriti è il primo: ${num1}`);
+    h1El.innerHTML = `Il maggiore tra i due numeri inseriti è il primo: ${num1}`;
 } else if (num1 < num2) {
     console.log(`Il maggiore tra i due numeri inseriti è il secondo: ${num2}`);
+    h1El.innerHTML = `Il maggiore tra i due numeri inseriti è il secondo: ${num2}`;
 } else {
     console.log(`I numeri sono uguali`);
+    h1El.innerHTML = `I numeri sono uguali`;
 }
